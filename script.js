fc969e755e9dcb5f5da99249b7947ddba7b08b2c
houleLHp5 <houlemattou.toure>
2020-11-10 22:06:12 +0100

    mon exercice 6 de la partie 5

---
+++ script.js
@@ -0,0 +1,30 @@
+/*focusMethod = function getFocus() {           
+    document.getElementById("inlineFormInput").focus();
+}*/
+
+/*function houleAddBorderName() {
+    document.getElementById("name").style.border = "6px solid red";
+function houleRemoveBorderName() {
+    document.getElementById("name").style.border = "";
+}
+
+function houleAddBordeMail() {
+    document.getElementById("mail").style.border = "6px solid red";
+
+function houleRemoveBorderMail() {
+    document.getElementById("mail").style.border = "";
+}*/
+
+function houleAddBorderName (element){
+    element.style.border = "6px solid red";
+}
+
+function houleRemoveBorderName (element){
+    element.style.border = "";
+}
+
+
+
+
+
+
